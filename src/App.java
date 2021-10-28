25d21ed7e02e1a92fc4ceaa060c80563bacb06c7
Adam0Schulz <adam@schulz.sk>
2021-10-28 14:46:40 +0200

    replaced scanners with console but still doesn't work

--- src/App.java
+++ src/App.java
@@ -1,17 +1,12 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.Scanner;
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.io.*;
 
 public class App implements Serializable {
 
     static Database database = load("database.ser");
     static String chooseSentence = "Please choose one of the following options:";
     static User currentUser;
+    static Console console = System.console();
 
     public static void main(String[] args) throws Exception {
 
@@ -44,7 +39,7 @@ public class App implements Serializable {
          */
         // System.out.println(database.getMovies().get(0).getTitle());
         // System.out.println(database.getUsers().get(1).getUsername());
-        start();
+        System.out.println("hi guys");
 
     }
 
@@ -56,18 +51,18 @@ public class App implements Serializable {
 
     public static void login() {
         System.out.println("Please enter your username: ");
-        String username = scannerString();
+        String username = console.readLine();
         currentUser = database.searchForUser(username);
         if (currentUser == null) {
             System.out
                     .println("Your have not created an account yet. In order to create an account create a password.");
-            String password = scannerString();
+            String password = console.readLine();
             database.addUser(new User(username, password));
             currentUser = database.searchForUser(username);
             menu();
         } else {
             System.out.println("Please enter your password: ");
-            String password = scannerString();
+            String password = console.readLine();
             if (currentUser.passwordValidation(password)) {
                 menu();
             } else {
@@ -81,25 +76,25 @@ public class App implements Serializable {
     public static void menu() {
         System.out.println(chooseSentence
                 + "(1) list all the movies, (2) search for a movie by title, (3) list all of your favourite movies");
-        if (scannerInt() == 1) {
+        if (Integer.parseInt(console.readLine()) == 1) {
             System.out.println("Enter the number of the coresponding movie: ");
             database.listMovies();
-            Movie movie = database.selectMovie(scannerInt());
+            Movie movie = database.selectMovie(Integer.parseInt(console.readLine()));
             movieMenu(movie);
-        } else if (scannerInt() == 2) {
+        } else if (Integer.parseInt(console.readLine()) == 2) {
             System.out.println("Enter the title of movie you want to search for: ");
-            Movie selectedMovie = database.searchForMovie(scannerString());
+            Movie selectedMovie = database.searchForMovie(console.readLine());
             if (selectedMovie == null) {
                 incorrectInput("movie title");
                 menu();
             } else {
                 movieMenu(selectedMovie);
             }
-        } else if (scannerInt() == 3) {
+        } else if (Integer.parseInt(console.readLine()) == 3) {
             System.out.println(
                     "This is your favourite list. You can play a movie by typing the coresponding number or you can go back by typing the number 0");
             currentUser.listFavourites();
-            int input = scannerInt();
+            int input = Integer.parseInt(console.readLine());
             if (input == 0) {
                 menu();
             } else {
@@ -119,12 +114,12 @@ public class App implements Serializable {
     public static void movieMenu(Movie movie) {
         System.out.println(chooseSentence + "(1) play " + movie.getTitle() + ", (2) add " + movie.getTitle()
                 + " to your favourite list");
-        if (scannerInt() == 1) {
+        if (Integer.parseInt(console.readLine()) == 1) {
             movie.playMovie();
             currentUser.addSeenMovie(movie);
             goBack();
 
-        } else if (scannerInt() == 2) {
+        } else if (Integer.parseInt(console.readLine()) == 2) {
             currentUser.addToFavouriteList(movie);
             menu();
         }
@@ -132,7 +127,7 @@ public class App implements Serializable {
 
     public static void goBack() {
         System.out.println("Enter the letter Q to go back to the main menu");
-        if (scannerString().equalsIgnoreCase("q")) {
+        if (console.readLine().equalsIgnoreCase("q")) {
             menu();
         } else {
             incorrectInput("input");
@@ -144,19 +139,15 @@ public class App implements Serializable {
         System.out.println("here is your list of your favourite movies");
     }
 
-    public static String scannerString() {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        scanner.close();
-        return input;
-    }
-
-    public static int scannerInt() {
-        Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
-        scanner.close();
-        return input;
-    }
+    /*
+     * public static String scannerString(Scanner scanner) { String input =
+     * scanner.nextLine(); return input; }
+     * 
+     * public static int scannerInt(Scanner scanner) { int input =
+     * scanner.nextInt(); return input; }
+     * 
+     * public static void closeScanner(Scanner scanner) { scanner.close(); }
+     */
 
     public static void incorrectInput(String input) {
         System.out.println("Incorrect " + input + ". Please try again.");
--- src/test.java
+++ src/test.java
@@ -1,7 +1,8 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
+        System.out.println("hello");
 
     }
 }
